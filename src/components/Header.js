4de891d2458f6e5db9e3f60e5757640663fc332a
Pedro Candido <pedrohcn02@gmail.com>
2022-02-10 17:53:15 -0300

    correção lint requisito 4 e 5

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,8 @@
-import React from "react";
-import { connect } from "react-redux";
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Header extends React.Component {
-  constructor() {
-    super()
-
-    this.state = {
-      total: 0,
-    }
-  }
   render() {
     const { userEmail, expenses, currencies } = this.props;
     return (
@@ -16,8 +10,8 @@ class Header extends React.Component {
         <h2 data-testid="email-field">{userEmail}</h2>
         <h4 data-testid="total-field">
           {expenses
-              .reduce((acc, expense) => acc + (
-                expense.value * currencies[expense.currency].ask), 0)}
+            .reduce((acc, expense) => acc + (
+              expense.value * currencies[expense.currency].ask), 0)}
         </h4>
         <h4 data-testid="header-currency-field">BRL</h4>
       </header>
@@ -30,5 +24,11 @@ const mapStateToProps = (state) => ({
   expenses: state.wallet.expenses,
   currencies: state.wallet.currencies,
 });
+
+Header.propTypes = {
+  userEmail: PropTypes.string,
+  expenses: PropTypes.arrayOf(PropTypes.any),
+  currencies: PropTypes.objectOf(PropTypes.any),
+}.isRequired;
 
 export default connect(mapStateToProps)(Header);
